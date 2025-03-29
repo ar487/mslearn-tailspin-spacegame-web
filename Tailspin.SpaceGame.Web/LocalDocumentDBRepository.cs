a235f67a3f56946d82eee6c082427e8972ec0744
Aritra <kusal.ch13@gmail.com>
2025-03-29 09:46:48 +0530

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -60,7 +60,7 @@ namespace TailSpin.SpaceGame.Web
                 .Where(queryPredicate) // filter
                 .OrderByDescending(orderDescendingPredicate) // sort
                 .Skip(page * pageSize) // find page
-                .Take(pageSize); // take items_modified
+                .Take(pageSize); // take items
 
             return Task<IEnumerable<T>>.FromResult(result);
         }
